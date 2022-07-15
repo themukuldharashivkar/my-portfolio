415b927129cc8ea331a4b53006431b15a2df7986
Mukul Dharashivkar <mukul.dharashivkar99@gmail.com>
2022-07-15 22:15:13 +0530

    Update Me.jsx

--- components/Me.jsx
+++ components/Me.jsx
@@ -47,7 +47,7 @@ const Me = () => {
 
           <Link href="/#portfolio">
             <div className="group flex items-center justify-center my-8 border-2 border-amber-500 bg-black text-white hover:text-amber-500 px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
-              my portfolio
+              portfolio
               <span className="-rotate-90 duration-100 ease-in group-hover:-rotate-180">
                 <MdExpandMore size={25} />
               </span>
