0cc98d6bc4a958dd89025f986a265b6958fcbae2
Mukul Dharashivkar <mukul.dharashivkar99@gmail.com>
2022-07-16 18:06:39 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -13,12 +13,12 @@ const About = () => {
               Who am I?
             </h2>
             <h4 className="py-4 max-w-2xl mx-20 font-header text-xl font-medium text-white sm:text-2xl lg:text-3xl">
-              I'm Mukul Dharashivkar, a Software Developer & Gamer
+              I am Mukul Dharashivkar, a Software Developer & Gamer
             </h4>
             <p className="py-4 max-w-2xl mx-20 text-white font-body leading-relaxed">
               I am a fresher in software development scene, I can write professional
               programs in Python, C and C++, Java, HTML, Javascript and CSS. I have done
-              my Bachelors of Engineeing from Progressive Education Society's Modern Colleges
+              my Bachelors of Engineeing from Progressive Education Society Modern Colleges
               of Engineering, Pune, Maharashtra -IN. I am just fascinated about the automation
               of tasks that coding brings into play. To sum it up, I LOVE CODING.
             </p>
