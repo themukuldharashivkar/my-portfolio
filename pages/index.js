d5ff23015a7ba305d3d2274eebf719ec6b13e176
Mukul Dharashivkar <mukul.dharashivkar99@gmail.com>
2022-07-16 18:10:24 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <div>
       <HomePage />
-      <Portfolio />
-      <Skills />
       <About />
+      <Skills />
+      <Portfolio />
       <Contact />
     </div>
   );
